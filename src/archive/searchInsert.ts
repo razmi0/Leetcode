5b29e95b6ddd745ab367eaf86e696fda7c977f16
razmio <razmio@razmio.razmio>
2023-09-09 03:53:59 +0200

    searchInsert

---
+++ src/archive/searchInsert.ts
@@ -0,0 +1,15 @@
+function searchInsert(nums: number[], target: number): number {
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.trunc((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    }
+  }
+  return left;
+}
