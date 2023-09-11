892aa4a5e7fbb6d8048b19adf3d48854d846bfcf
razmio <razmio@razmio.razmio>
2023-09-11 18:43:41 +0200

    trash has spam & isPalindrome prototype

--- src/Trash.ts
+++ src/Trash.ts
@@ -32,4 +32,6 @@ class Trash {
   }
 }
 
+// ⧹╲⎝⧹༼◕ ͜oﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞﱞ.◕ ༽⧸⎠╱⟋╱
+
 export default Trash;
--- src/archive/isPalindrome.ts
+++ src/archive/isPalindrome.ts
@@ -1,14 +1,29 @@
 declare global {
   interface Number {
-    isPalindrome(): boolean;
+    isPalindrome(this: number): boolean;
   }
   interface NumberConstructor {
-    isPalindrome(): boolean;
+    isPalindrome(this: number): boolean;
+  }
+  interface String {
+    isPalindrome(this: string): boolean;
+  }
+  interface StringConstructor {
+    isPalindrome(this: string): boolean;
   }
 }
 
-Number.prototype.isPalindrome = function (): boolean {
+Number.prototype.isPalindrome = function (this: number): boolean {
   return Number(this.toString().split("").reverse().join("")) === this;
 };
 
-console.log((121).isPalindrome());
+String.prototype.isPalindrome = function (this: string): boolean {
+  let s = this.toLowerCase();
+  const arr = s.split("").filter((char) => /[A-Za-z0-9]/.exec(char));
+  for (let i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+    if (arr[i] !== arr[j]) return false;
+  }
+  return true;
+};
+
+export {};
