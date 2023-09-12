09949a8909e0b02bcc5c7efb25f60bbb67e2ea6b
razmio <razmio@razmio.razmio>
2023-09-12 18:52:42 +0200

    containsNearbyDuplicate

---
+++ src/archive/containsNearbyDuplicate.ts
@@ -0,0 +1,10 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
