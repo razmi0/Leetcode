48e800d8a0f0cd9781c2b49720fa96b404d5a3d4
razmio <razmio@razmio.razmio>
2023-09-12 17:39:41 +0200

    isIsomorphic

---
+++ src/archive/isIsomorphic.ts
@@ -0,0 +1,19 @@
+function isIsomorphic(s: string, t: string) {
+  if (s.length != t.length) {
+    return false;
+  }
+  const mapS = new Map();
+  const mapT = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    if (mapS.get(s[i]) != mapT.get(t[i])) {
+      return false;
+    }
+    mapS.set(s[i], i);
+    mapT.set(t[i], i);
+  }
+
+  return true;
+}
+
+//
