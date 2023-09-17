c2fd699ec67df95b2c0c747e9983cda214eeff18
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-09-17 23:35:50 +0200

    feat : beautify & n is process var

--- src/exploration/notes.ts
+++ src/exploration/notes.ts
@@ -2,26 +2,37 @@
 // Bitwise operators & bit manipulation
 // Operators: & | ^ ~ << >> >>>
 
-const n = 4;
-console.log("multiplication by 2  : " + (n << 1));
-console.log("multiplication by 4  : " + (n << 2));
-console.log("multiplication by 8  : " + (n << 3));
-console.log("multiplication by 16 : " + (n << 4));
+import chalk from "chalk";
+
+const n = 12;
+console.log(chalk.red("n = " + n));
+console.log(chalk.blue("multiplication "));
+console.log(chalk.green("by 2 : ") + chalk.yellow(n << 1));
+console.log(chalk.green("by 4 : ") + chalk.yellow(n << 2));
+console.log(chalk.green("by 8 : ") + chalk.yellow(n << 3));
+console.log(chalk.green("by 16 : ") + chalk.yellow(n << 4));
 
 //
-console.log("division by 2 : " + (n >> 1));
-console.log("division by 2 (without sign) : " + (n >>> 1));
-console.log("division by 4 : " + (n >> 2));
-console.log("division by 4 (without sign) : " + (n >>> 2));
-console.log("check if number is odd : " + (n & 1));
-console.log("check if number is even : " + (n & 2));
-console.log("set number to odd : " + (n | 1));
-console.log("set number to even : " + (n & ~1));
+console.log(chalk.blue("division ")); // >>> for unsigned
+console.log(chalk.green("by 2 : ") + chalk.yellow(n >> 1));
+console.log(chalk.green("by 4 : ") + chalk.yellow(n >> 2));
+console.log(chalk.green("by 8 : ") + chalk.yellow(n >> 3));
+console.log(chalk.green("by 16 : ") + chalk.yellow(n >> 4));
+
+console.log(chalk.blue("odd or even ? "));
+let a = (n & 1) == 0;
+let b = (n & 0) == 0;
+console.log(chalk.green("odd : ") + chalk.yellow(a));
+console.log(chalk.green("even : ") + chalk.yellow(b));
+
+console.log(chalk.blue("set to odd or even "));
+console.log(chalk.green("set number to odd : ") + chalk.yellow(n | 1));
+console.log(chalk.green("set number to even : ") + chalk.yellow(n & ~1));
 
 // ^
-console.log("toggle the 3rd bit : " + (n ^ 4));
-console.log("toggle the 3rd bit : " + (n ^ 8));
-console.log("toggle the 3rd bit : " + (n ^ 16));
+console.log(chalk.blue("toggle bits"));
+console.log(chalk.green("toggle bit 1 : ") + chalk.yellow(n ^ 1));
+console.log(chalk.green("toggle bit 2 : ") + chalk.yellow(n ^ 2));
 
 // Looping multiple times over object/array
 // let i = 0,
