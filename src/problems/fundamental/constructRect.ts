48d358b4786d36fd5b3631be35995da5d76ec352
razmio <razmio@razmio.razmio>
2023-09-27 14:47:43 +0200

    constructRect runtime / 2

--- src/problems/fundamental/constructRect.ts
+++ src/problems/fundamental/constructRect.ts
@@ -1,4 +1,4 @@
-function constructRectangle(area: number): number[] {
+function constructRectangle2(area: number): number[] {
   let sqRoot = Math.sqrt(area);
   if (Number.isInteger(sqRoot)) return [sqRoot, sqRoot];
   else {
@@ -17,4 +17,12 @@ function constructRectangle(area: number): number[] {
     }
     return sizes;
   }
+}
+
+function constructRectangle(area: number): number[] {
+  const sqrt = Math.floor(Math.sqrt(area));
+  for (let i = sqrt; i >= 1; i--) {
+    if (area % i == 0) return [area / i, i];
+  }
+  return [area, 1];
 }
